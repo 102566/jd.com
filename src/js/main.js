20a6ee5e46c4fd273d6ffc75b94b00be0e3585c6
chq <664985332@qq.com>
2020-06-29 22:38:17 +0800

    v1.1.2

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,17 @@ requirejs.config({
         md5: './jquery.md5'
     }
 })
-require(['jquery'], function() {
-        $('.cate_menu_item').hover(function() {
-            let _index = $('.cate_menu_item').index(this);
-            $('#J_phpCtn').css({
-                display: "block"
-            });
-            $('#cate_item' + (_index + 1)).css({
-                display: "block"
-            })
-        })
-    },
-    function() {
-        $('#J_phpCtn').css({
-            display: "none"
-        });
-        $('#cate_item' + (_index + 1)).css({
-            display: "none"
-        })
-    }
-)
+require(['jquery'], function($) {
+    var _index;
+    $('.cate_menu_item').on("mouseover", function() {
+        $('.cate_part').css("display", "none");
+        _index = $('.cate_menu_item').index(this);
+        $('#J_popCtn').css("display", "block");
+        $('#cate_item' + (_index + 1)).css("display", "block");
+    })
+
+    $('.fs_inner').on("mouseleave", function() {
+        $('#J_popCtn').css("display", "none");
+        $('#cate_item' + (_index + 1)).css("display", "none");
+    })
+})
